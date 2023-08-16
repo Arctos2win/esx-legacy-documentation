f7b4e10d9d25d98a4c859c5f1461770ba46389cc
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2023-08-16 17:52:21 +0200

    feat(theme config): added dynamic logo
    added dynamic changing logo with theme switching (gotta test if the "next-themes" package really needs to be npm installed since logic worked even before installation)

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,32 +1,78 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useTheme } from 'next-themes'
+import { useState, useEffect } from 'react'
+
+function ThemedImage() {
+  const { resolvedTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  var src
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  if (!mounted) {
+    return null
+  }
+
+  switch (resolvedTheme) {
+    case 'light':
+      src = <div 
+        style={{
+          paddingLeft: '130px', 
+          lineHeight: '118px', 
+          backgroundImage: `url('https://media.discordapp.net/attachments/1141377366589976747/1141377486089896027/dark.png?width=593&height=593')`, 
+          backgroundRepeat:  "no-repeat", 
+          backgroundSize: '120px',
+        }} suppressHydrationWarning = {true}> ESX Documentation</div>
+      break
+    case 'dark':
+      src = <div 
+        style={{
+          paddingLeft: '130px', 
+          lineHeight: '118px', 
+          backgroundImage: `url('https://media.discordapp.net/attachments/989963898570551296/1141297556555579443/white-800x800.png?width=593&height=593')`, 
+          backgroundRepeat:  "no-repeat", 
+          backgroundSize: '120px',
+        }} suppressHydrationWarning = {true}> ESX Documentation</div>
+      
+      break
+  }
+    return src
+}
+
+
 
 const config: DocsThemeConfig = {
-  primaryHue: {
-    dark: 30,
-    light: 30,
-  },
-  logo: <span className="nx-font-bold">Fivemanage</span>,
+  
+  logo: ThemedImage,
+
   project: {
-    link: 'https://github.com/fivemanage/docs',
+    link: 'https://google.com',
   },
   chat: {
-    link: 'https://discord.gg/wUgAGHnucZ',
+    link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/fivemanage/docs',
   footer: {
-    text: 'Fivemanage Docs',
+    text:
+      <span>
+           {new Date().getFullYear()} ©{' '}
+          <a href="https://www.esx-framework.org/" target="_blank">
+            ESX Framework
+          </a>
+        </span>
   },
   useNextSeoProps() {
     return {
-      title: 'Fivemanage Docs',
-      description: 'Fivemanage Docs',
+      title: 'ESX Documentation',
+      description: 'ESX Documentation',
       openGraph: {
         type: 'website',
         locale: 'en_US',
         url: 'https://docs.fivemanage.com',
-        title: 'Fivemanage Docs',
-        description: 'Fivemanage Docs',
+        title: 'ESX Documentation',
+        description: 'ESX Documentation',
       },
       twitter: {
         handle: '@fivemanage',
@@ -42,9 +88,16 @@ const config: DocsThemeConfig = {
             name: 'keywords',
             content: 'fivemanage, docs, documentation, fivemanage docs, fivemanage documentation, fivem analytics, fivem, fivem server, fivem server analytics, fivem server management, fivem server monitoring, fivem server monitoring, fivem server management',
         },
+      ],
+      additionalLinkTags : [
+        {
+          rel: 'icon',
+          href: 'https://media.discordapp.net/attachments/989963898570551296/1141265486902997044/esx.e214a7a0-removebg-preview.png?width=732&height=275',
+        },
       ]
     }
   }
 }
 
 export default config
+
