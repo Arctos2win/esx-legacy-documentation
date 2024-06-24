ae613fc8b0493cedea05baee7a89de57b71b1a5c
Gellipapa <gellerthorvath0@gmail.com>
2024-06-24 22:23:25 +0200

    :pencil2: fix typo

--- theme.config.tsx
+++ theme.config.tsx
@@ -104,7 +104,7 @@ const config: DocsThemeConfig = {
       return (
         <span>
           <a href="https://www.esx-framework.org/" target="_blank">
-            {t.copyright} {new Date().getFullYear()} ESX. {t.allRightsReserved}.
+            {t.copyright} {new Date().getFullYear()} ESX. {t.allRightsReserved}
           </a>
           <br></br>
           <a href="https://www.oxygenserv.com/en/" target="_blank">
