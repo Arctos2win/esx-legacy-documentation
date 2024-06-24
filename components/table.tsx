b8188d1042eb086d90d486bde12b72e6760ab2f4
Gellipapa <gellerthorvath0@gmail.com>
2024-06-24 21:37:07 +0200

    :ambulance: Fix table design issues in v3, change nx to _

--- components/table.tsx
+++ components/table.tsx
@@ -1,22 +1,22 @@
 export const ArgumentsTable = ({ rows, isTable }) =>(
-    <table className="nx-block nx-overflow-x-scroll nextra-scrollbar nx-mt-6 nx-p-0 first:nx-mt-0">
+    <table className="_block _overflow-x-scroll nextra-scrollbar _mt-6 _p-0 first:_mt-0">
         <thead>
-            <tr className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20">
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">{isTable ? "Key" : "Argument"}</th>
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">Data Type</th>
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">Optional</th>
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">Default Value</th>
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">Description</th>
+            <tr className="_m-0 _border-t _border-gray-300 _p-0 dark:_border-gray-600 even:_bg-gray-100 even:dark:_bg-gray-600/20">
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">{isTable ? "Key" : "Argument"}</th>
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">Data Type</th>
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">Optional</th>
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">Default Value</th>
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">Description</th>
             </tr>
         </thead>
         <tbody>
             {rows.map(row => 
-                <tr className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20" key={row.arg}>
-                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">{row.arg}</td>
-                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 nx-whitespace-nowrap">{row.type || "any"}</td>
-                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">{row.optional ? "Yes" : "No"}</td> 
-                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">{row.defaultValue}</td>
-                <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">{row.description}</td>
+                <tr className="_m-0 _border-t _border-gray-300 _p-0 dark:_border-gray-600 even:_bg-gray-100 even:dark:_bg-gray-600/20" key={row.arg}>
+                <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600">{row.arg}</td>
+                <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600 _whitespace-nowrap">{row.type || "any"}</td>
+                <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600">{row.optional ? "Yes" : "No"}</td> 
+                <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600">{row.defaultValue}</td>
+                <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600">{row.description}</td>
             </tr>
             )}
         </tbody>
@@ -24,20 +24,20 @@ export const ArgumentsTable = ({ rows, isTable }) =>(
 );
 
 export const OutputTable = ({ rows, isTable }) =>(
-    <table className="nx-block nx-overflow-x-scroll nextra-scrollbar nx-mt-6 nx-p-0 first:nx-mt-0">
+    <table className="_block _overflow-x-scroll nextra-scrollbar _mt-6 _p-0 first:_mt-0">
         <thead>
-            <tr className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20">
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">{isTable ? "Key" : "Value"}</th>
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">Data Type</th>
-                <th className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 nx-font-semibold dark:nx-border-gray-600">Description</th>
+            <tr className="_m-0 _border-t _border-gray-300 _p-0 dark:_border-gray-600 even:_bg-gray-100 even:dark:_bg-gray-600/20">
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">{isTable ? "Key" : "Value"}</th>
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">Data Type</th>
+                <th className="_m-0 _border _border-gray-300 _px-4 _py-2 _font-semibold dark:_border-gray-600">Description</th>
             </tr>
         </thead>
         <tbody>
             {rows.map(row => 
-                <tr className="nx-m-0 nx-border-t nx-border-gray-300 nx-p-0 dark:nx-border-gray-600 even:nx-bg-gray-100 even:dark:nx-bg-gray-600/20" key={row.arg}>
-                    <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">{row.arg}</td>
-                    <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600 nx-whitespace-nowrap">{row.type || "any"}</td>
-                    <td className="nx-m-0 nx-border nx-border-gray-300 nx-px-4 nx-py-2 dark:nx-border-gray-600">{row.description}</td>
+                <tr className="_m-0 _border-t _border-gray-300 _p-0 dark:_border-gray-600 even:_bg-gray-100 even:dark:_bg-gray-600/20" key={row.arg}>
+                    <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600">{row.arg}</td>
+                    <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600 _whitespace-nowrap">{row.type || "any"}</td>
+                    <td className="_m-0 _border _border-gray-300 _px-4 _py-2 dark:_border-gray-600">{row.description}</td>
                 </tr>
             )}
         </tbody>
