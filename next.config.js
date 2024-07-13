bddea453195288a9065c24a403de41a411e017d5
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2024-07-13 12:35:14 +0100

    [IMP] Changed app and document extensions to typescript, cleaner next config without permanent redirect, now its handled by app useEffect and also handles incorrect locales in path

--- next.config.js
+++ next.config.js
@@ -1,37 +1,32 @@
-import nextra from 'nextra'
-import remarkMdxDisableExplicitJsx from 'remark-mdx-disable-explicit-jsx';
+import nextra from "nextra";
+import remarkMdxDisableExplicitJsx from "remark-mdx-disable-explicit-jsx";
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './src/theme.config.tsx',
+  theme: "nextra-theme-docs",
+  themeConfig: "./src/theme.config.tsx",
 
   mdxOptions: {
     remarkPlugins: [
       [
         remarkMdxDisableExplicitJsx,
-        { whiteList: ['table', 'thead', 'tbody', 'tr', 'th', 'td'] }
-      ]
-    ]
-  }
-
-})
-
+        { whiteList: ["table", "thead", "tbody", "tr", "th", "td"] },
+      ],
+    ],
+  },
+});
 
 /**
  * @type {import('next').NextConfig}
  */
 export default withNextra({
-    redirects: () => [
-      {
-        source: '/',
-        destination: '/en',
-        permanent: true
-      }
-    ],
-    images: {
-      unoptimized: true,
-    },
-    swcMinify: true,
-    trailingSlash: true,
-    distDir: 'dist'
-  })
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
+  images: {
+    unoptimized: true,
+  },
+  swcMinify: true,
+  trailingSlash: true,
+  distDir: "dist",
+});
