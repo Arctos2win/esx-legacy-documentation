f7fb0b04bf812e7d4763b2a560739daa12b064da
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2024-07-13 12:56:29 +0100

    [ADD] Added custom useRedirector hook to take care of locale redirecting, also returns a loading boolean and a verifyLocale function for better reusability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,7 @@
-import { useEffect } from "react";
-import { useRouter } from "nextra/hooks";
-import { SUPPORTED_LOCALES } from "../data/translations";
+import { useRedirector } from "src/hooks/useRedirector";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const browserLang = navigator.language || "en";
-    const supportedLocales = SUPPORTED_LOCALES.map((lang) => lang.locale);
-
-    let detectedLocale = "en";
-    const browserLangSplit = browserLang.split("-");
-
-    if (browserLangSplit.length > 0) {
-      const currentLang = browserLangSplit[0];
-
-      if (supportedLocales.includes(currentLang)) {
-        detectedLocale = currentLang;
-      }
-    }
-
-    const currentPath = router.asPath;
-
-    // Extract the current locale from the path
-    const pathParts = currentPath.split("/");
-    const currentLocale = pathParts[1];
-
-    if (!supportedLocales.includes(currentLocale)) {
-      // If no valid locale in path, prepend the detected locale
-      const newPath = `/${detectedLocale}${currentPath}`;
-      router.replace(newPath);
-    }
-  }, [router]);
+  const { loading, verifyLocale } = useRedirector();
 
   return <Component {...pageProps} />;
 }
