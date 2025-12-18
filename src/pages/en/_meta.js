0db1e57cd64e2fe3af06553ed965e622ce74d751
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2025-12-18 23:23:29 +0100

    feat: ✨ Add plebmasters partnership

--- src/pages/en/_meta.js
+++ src/pages/en/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: {
     title: "Introduction",
   },
+  partnerships: "Partnerships",
   syndicate: "ESX: Syndicate Roleplay",
   esx_core: "ESX: Legacy (Core)",
   esx_addons: "ESX: Legacy (Addons)",
